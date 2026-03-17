6f71c4de8063160598b6601efef8221aa7f25659
Samundra Poudel <samundrapoudel@Samundras-MacBook-Air.local>
2026-03-17 21:49:55 +0530

    test: breaking all rules

--- author.py
+++ author.py
@@ -1,7 +1,8 @@
-def login(password):
-
-    secret_key = "12345-ABCD-Secret" 
-    if password == secret_key:
-        print("Access Granted")
-    else:
-        print("Access Denied")
+# Violates Rule: Missing Type Hints
+# Violates Rule: SQL Injection (String Concat)
+# Violates Rule: Hardcoded Secret
+def connect_to_db(user_input):
+    db_password = "super-secret-password-123"
+    query = "SELECT * FROM users WHERE name = '" + user_input + "'"
+    print(f"Connecting with {db_password} to run {query}")
+    return query
